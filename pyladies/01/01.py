71720b12e37958552373d050865e530a87502837
prednosta <bretakral@gmail.com>
2018-04-01 10:13:52 +0200

    prvni priklady pyladies

---
+++ pyladies/01/01.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+print("hello world")
+
+print("dp.1.0")
+print("1 + 1: ", 1 + 1)
+
+print(" ")
+
+print("dp.1.1")
+print("4 - 2: ", 4 - 2)
+
+print(" ")
+
+print("dp.1.2")
+print("4 * 2: ", 4 * 2)
+
+print(" ")
+
+print("dp.1.3")
+print("4 / 2: ", 4 / 2)
+
+print(" ")
+
+print("dp.1.4")
+print("3 + ( 4 + 6 ) * 8 / 2 - 1: ", 3 + ( 4 + 6 ) * 8 / 2 - 1)
+
+print(" ")
+
+print("dp.1.5")
+print("4 % 2: ", 4 % 2, "zbytek po deleni")
+print("5 % 2: ", 5 % 2, "zbytek po deleni")
+
+print(" ")
+
+print("dp.1.6")
+print("4 ** 2: ", 4 ** 2, "mocnina")
+
+print(" ")
+
+print("dp.1.9")
+print("A+B: ", 'A' + "B")
+
+print(" ")
+
+print("dp.1.10")
+print('Ahoj' + 'PyLadies')
+print('Ahoj ' + 'PyLadies')
+
+print(" ")
+
+print("dp.1.11")
+print("A * B ", "noooope")
+print("A / B ", "noooope")
+print("A - B ", "noooope")
+
+print(" ")
+
+print("dp.1.12")
+print("A + 5 ", "cant convert A to int")
+
+print(" ")
+
+print("dp.1.13")
+print("A * 5 ", "A" * 5)
+
+print("end of story")
